6ca31c6f10bf4afa6517d4d3b918f3849f63399f
RolandSamidu <73852286+RolandSamidu@users.noreply.github.com>
2021-07-07 14:04:24 +0530

    Calculate Interest
    Program to Find Simple Interest and Compound Interest.

---
+++ Example_Codes/program_07.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class CalculateInterest {
+    public static void main(String[] args) {
+        double p, n, r, si, ci;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the amount : ");
+        p = s.nextDouble();
+
+        System.out.print("Enter the No.of years : ");
+        n = s.nextDouble();
+
+        System.out.print("Enter the rate of interest : ");
+        r = s.nextDouble();
+
+        si = (p * n * r) / 100;
+        ci = p * Math.pow(1.0 + r / 100.0, n) - p;
+
+        System.out.println("\nAmount : " + p);
+        System.out.println("No. of years : " + n);
+        System.out.println("Rate of interest : " + r);
+        System.out.println("Simple Interest : " + si);
+        System.out.println("Compound Interest : " + ci);
+    }
+}
