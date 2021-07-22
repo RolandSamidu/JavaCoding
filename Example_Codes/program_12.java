bda6478cd94d4d7e6f9bbd5687676e793da8b63f
RolandSamidu <73852286+RolandSamidu@users.noreply.github.com>
2021-07-22 22:28:51 +0530

    BinaryToOctal
    Program to Convert Binary to Octal.

---
+++ Example_Codes/program_12.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class BinaryToOctal {
+    public static void main(String[] args) throws Exception{
+        String num = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter Binary number : ");
+        num = br.readLine();
+        int dec = Interger.parseInt(num,2);
+
+        String oct = Integer.toOctalString(dec);
+
+        System.out.println("Binary " + num + " in Octal is " + oct);
+    }
+}
