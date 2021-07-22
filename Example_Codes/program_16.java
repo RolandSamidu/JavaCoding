de56f951bc4b51b22da82b8e5c49ff330093cc5c
RolandSamidu <73852286+RolandSamidu@users.noreply.github.com>
2021-07-22 22:40:44 +0530

    Harmonic Series
    Program to Find Harmonic Series.

---
+++ Example_Codes/program_16.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class HarmonicSeries {
+
+    public static void main(String args[]) {
+
+        int num, i = 1;
+        double rst = 0.0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number for length of series");
+        num = in.nextInt();
+
+        while (i <= num) {
+
+            System.out.print("1/" + i + " +");
+            rst = rst + (double) 1 / i;
+
+            i++;
+        }
+
+        System.out.println("\n\nSum of Harmonic Series is " + rst);
+    }
+}
