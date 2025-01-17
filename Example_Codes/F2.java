dde7f1c62056ee3c87934dcbb2626aa7f45fa88c
RolandSamidu <samidu.appuhamy@gmail.com>
2025-01-17 23:10:27 +0530

    Added new code for design Alphabet Pattern

--- Example_Codes/F2.java
+++
@@ -1,9 +0,0 @@
-
-public class F2 {
-
-    public void option() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'option'");
-    }
-
-}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # JavaCoding
 This repository gives you all the code and programs in the JAVA programming language.
+
+21. Alphabet Pattern
+A 
+BB 
+CCC
+DDDD
+EEEEE 
+
+
