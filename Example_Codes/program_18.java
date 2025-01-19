86749202aa2a4c5dc93356b93618abcfa4f90ce0
RolandSamidu <samidu.appuhamy@gmail.com>
2025-01-19 23:52:32 +0530

    Add Algorithm for Floyd Triangle from numbers

--- Example_Codes/program_18.java
+++ Example_Codes/program_18.java
@@ -2,10 +2,10 @@ class BinaryPattern {
     public static void main(String s[]) {
         int i, j;
         int count = 1;
-        for (i = 1; i <= 5; i++) {
+        for (i = 1; i <= 10; i++) {
             for (j = 1; j <= i; j++) {
                 System.out.format("%d", count++ % 2);
-                if (j == i && i != 5)
+                if (j == i && i != 10)
                     System.out.println("");
             }
 
