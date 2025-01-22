9a7258887c10c2558272785f212e43bdb703aee0
RolandSamidu <samidu.appuhamy@gmail.com>
2025-01-22 23:48:29 +0530

    Update the EmployeeManagement class

--- EmployeeManagemet/EmployeeManagement.java
+++ EmployeeManagemet/EmployeeManagement.java
@@ -3,49 +3,6 @@ package EmployeeManagemet;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Employee {
-    private int employeeNumber;
-    private String employeeName;
-    private int attendance; // Number of days present
-    private double dailySalary;
-
-    public Employee(int employeeNumber, String employeeName, int attendance, double dailySalary) {
-        this.employeeNumber = employeeNumber;
-        this.employeeName = employeeName;
-        this.attendance = attendance;
-        this.dailySalary = dailySalary;
-    }
-
-    public int getEmployeeNumber() {
-        return employeeNumber;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public int getAttendance() {
-        return attendance;
-    }
-
-    public void setAttendance(int attendance) {
-        this.attendance = attendance;
-    }
-
-    public double calculateSalary() {
-        return attendance * dailySalary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeeNumber: " + employeeNumber + ", Name: " + employeeName + ", Attendance: " + attendance + ", Daily Salary: " + dailySalary;
-    }
-}
-
 public class EmployeeManagement {
     private static ArrayList<Employee> employeeList = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
